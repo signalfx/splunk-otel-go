284bb4aec8a0071e0542fb21da4b83b26d87387e
Tyler Yahn <codingalias@gmail.com>
2021-02-12 13:22:53 -0800

    Add example test

---
+++ otel/example_test.go
@@ -0,0 +1,32 @@
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package otel_test
+
+import (
+	"context"
+
+	"github.com/signalfx/splunk-otel-go/otel"
+)
+
+func Example() {
+	// By default, the Run function will create a Jaeger exporter to a locally
+	// running Splunk Smart Agent at http://localhost:9080 and will configure
+	// the B3 context propagation format to be used in extracting and
+	// injecting trace context.
+	sdk := otel.Run()
+	// To ensure all spans are flushed before the application exits, make sure
+	// to shutdown.
+	defer sdk.Shutdown(context.Background())
+}
