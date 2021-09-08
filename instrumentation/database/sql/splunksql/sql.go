0c5306e9a8ad3a3ca3976bf140d9385ade763d33
Tyler Yahn <codingalias@gmail.com>
2021-09-08 11:16:19 -0700

    Add copyright notice to all source files

--- instrumentation/database/sql/splunksql/sql.go
+++ instrumentation/database/sql/splunksql/sql.go
@@ -1,8 +1,22 @@
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package splunksql provides functions to trace the database/sql package
 // (https://golang.org/pkg/database/sql) using the OpenTelemetry API. It will
 // automatically augment operations such as connections, statements and
 // transactions with tracing.
-package splunksql
+package splunksql // import "github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
 
 import (
 	"context"
--- instrumentation/database/sql/splunksql/transport/transport.go
+++ instrumentation/database/sql/splunksql/transport/transport.go
@@ -1,6 +1,20 @@
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package transport provides identifiers for communication transport
 // protocols conforming to OpenTelemetry semantic conventions.
-package transport
+package transport // import "github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql/transport"
 
 import (
 	"go.opentelemetry.io/otel/attribute"
