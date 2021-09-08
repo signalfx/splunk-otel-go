9148a2c5cadc3ff20c0234e4bf8f37746248819a
Tyler Yahn <codingalias@gmail.com>
2021-09-08 11:01:47 -0700

    Add package documentation for the test package

---
+++ instrumentation/database/sql/splunksql/test/doc.go
@@ -0,0 +1,7 @@
+/*
+Package test validates the splunksql instrumentation with the default SDK.
+This package is in a separate module from the instrumentation it tests to
+isolate the dependency of the default SDK and not impose this as a transitive
+dependency for users.
+*/
+package test
