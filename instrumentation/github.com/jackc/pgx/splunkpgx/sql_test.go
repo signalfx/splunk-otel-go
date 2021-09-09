cc97bde132217f9f42fb803b7658b656b5df56c8
Tyler Yahn <codingalias@gmail.com>
2021-09-09 16:15:15 -0700

    pgx driver instrumentation PoC

---
+++ instrumentation/github.com/jackc/pgx/splunkpgx/sql_test.go
@@ -0,0 +1,85 @@
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package splunkpgx_test
+
+import (
+	"testing"
+
+	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
+	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql/transport"
+	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/jackc/pgx/splunkpgx"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDSNParser(t *testing.T) {
+	testcases := []struct {
+		name    string
+		dsn     string
+		connCfg splunksql.ConnectionConfig
+		errStr  string
+	}{
+		{
+			name:   "invalid dsn",
+			dsn:    "invalid dsn",
+			errStr: "invalid dsn",
+		},
+		{
+			name: "db name with defaults",
+			dsn:  "postgres:///testdb",
+			connCfg: splunksql.ConnectionConfig{
+				Name:             "testdb",
+				ConnectionString: "postgres:///testdb",
+				Host:             "localhost",
+				Port:             5432,
+				Transport:        transport.TCP,
+			},
+		},
+		{
+			name: "url: tcp address",
+			dsn:  "postrgres://user:password@localhost:8080/testdb",
+			connCfg: splunksql.ConnectionConfig{
+				Name:             "testdb",
+				ConnectionString: "postrgres://user@localhost:8080/testdb",
+				User:             "user",
+				Host:             "localhost",
+				Port:             8080,
+				Transport:        transport.TCP,
+			},
+		},
+		{
+			name: "params: unix socket",
+			dsn:  "user=user password=password host=/tmp/pgdb dbname=testdb",
+			connCfg: splunksql.ConnectionConfig{
+				Name:             "testdb",
+				ConnectionString: "user=user host=/tmp/pgdb dbname=testdb",
+				User:             "user",
+				Host:             "/tmp/pgdb",
+				Transport:        transport.Unix,
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := splunkpgx.DSNParser(tc.dsn)
+			if tc.errStr != "" {
+				assert.EqualError(t, err, tc.errStr)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tc.connCfg, got)
+		})
+	}
+}
