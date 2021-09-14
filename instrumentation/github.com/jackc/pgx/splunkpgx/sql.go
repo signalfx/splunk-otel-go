487555c9c4ce810677c370588c0bc977f84c06c3
Tyler Yahn <MrAlias@users.noreply.github.com>
2021-09-14 15:05:37 -0700

    Update instrumentation/github.com/jackc/pgx/splunkpgx/sql.go
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- instrumentation/github.com/jackc/pgx/splunkpgx/sql.go
+++ instrumentation/github.com/jackc/pgx/splunkpgx/sql.go
@@ -45,7 +45,7 @@ import (
 
 	"github.com/jackc/pgx"
 	// Make sure to import this so the instrumented driver is registered.
-	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql/dbsystem"
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql/transport"
