9ff605c5c17df63fd144807ae103dfeba4cb793e
Tyler Yahn <codingalias@gmail.com>
2021-09-22 10:41:33 -0700

    Update example_test with more complete example

--- instrumentation/github.com/go-sql-driver/mysql/splunkmysql/example_test.go
+++ instrumentation/github.com/go-sql-driver/mysql/splunkmysql/example_test.go
@@ -15,18 +15,93 @@
 package splunkmysql_test
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/signalfx/splunk-otel-go/distro"
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
 	_ "github.com/signalfx/splunk-otel-go/instrumentation/github.com/go-sql-driver/mysql/splunkmysql"
 )
 
+type server struct {
+	DB *sql.DB
+}
+
+func (s *server) listenAndServe() error {
+	// Requests to /square/n will return the square of n.
+	http.HandleFunc("/square/", s.handle)
+	return http.ListenAndServe(":80", nil)
+}
+
+func (s *server) handle(w http.ResponseWriter, req *http.Request) {
+	idx := strings.LastIndex(req.URL.Path, "/")
+	n, err := strconv.Atoi(req.URL.Path[idx+1:])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	query := "SELECT squareNumber FROM squarenum WHERE number = ?"
+	// Prepare a statement for reading data. It is important to include the
+	// passed context to ensure if there are any existing trace the spans
+	// created for the database interaction are appropriately included in that
+	// trace.
+	stmt, err := s.DB.PrepareContext(req.Context(), query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	var nSquared int
+	// Again, pass the context here to ensure spans are not orphaned.
+	err = stmt.QueryRowContext(req.Context(), n).Scan(&nSquared)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%d", nSquared)
+}
+
 func Example() {
+	// Setup the Splunk OTel Go distribution.
+	sdk, err := distro.Run()
+	if err != nil {
+		panic(err)
+	}
+	// Ensure all spans are flushed before the application exits.
+	defer func() {
+		if err := sdk.Shutdown(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Create a traced connection to the MySQL database.
 	db, err := splunksql.Open("mysql", "user:password@/dbname")
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
+	// Set recommended default DB configuration.
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
+
+	// Validate DSN data by opening a connection. There is no parent context
+	// to pass here so the span created from this operation will be in its own
+	// trace.
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	srv := &server{DB: db}
+	if err := srv.listenAndServe(); err != nil {
+		panic(err)
+	}
 }
