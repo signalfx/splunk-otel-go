4f867abf99ccb4eb98f1f8ed8790f1b4b21a3894
Tyler Yahn <codingalias@gmail.com>
2021-09-24 08:14:03 -0700

    Update example to be complete

--- instrumentation/github.com/jackc/pgx/splunkpgx/example_test.go
+++ instrumentation/github.com/jackc/pgx/splunkpgx/example_test.go
@@ -15,17 +15,79 @@
 package splunkpgx_test
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/signalfx/splunk-otel-go/distro"
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
 	// Make sure to import this so the instrumented driver is registered.
 	_ "github.com/signalfx/splunk-otel-go/instrumentation/github.com/jackc/pgx/splunkpgx"
 )
 
+type server struct {
+	DB *sql.DB
+}
+
+func (s *server) listenAndServe() error {
+	// Requests to /square/n will return the square of n.
+	http.HandleFunc("/square/", s.handle)
+	return http.ListenAndServe(":80", nil)
+}
+
+func (s *server) handle(w http.ResponseWriter, req *http.Request) {
+	idx := strings.LastIndex(req.URL.Path, "/")
+	n, err := strconv.Atoi(req.URL.Path[idx+1:])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	query := "SELECT squareNumber FROM squarenum WHERE number = ?"
+	var nSquared int
+	// Propagate the context to ensure created spans are included in any
+	// existing trace.
+	if err := s.DB.QueryRowContext(req.Context(), query, n).Scan(&nSquared); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%d", nSquared)
+}
+
 func Example() {
+	// Setup the Splunk OTel Go distribution.
+	sdk, err := distro.Run()
+	if err != nil {
+		panic(err)
+	}
+	// Ensure all spans are flushed before the application exits.
+	defer func() {
+		sErr := sdk.Shutdown(context.Background())
+		if sErr != nil {
+			panic(sErr)
+		}
+	}()
+
+	// Create a traced connection to the Postgres database.
 	db, err := splunksql.Open("pgx", "postgres://localhost:5432/dbname")
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
+
+	// Validate DSN data by opening a connection. There is no parent context
+	// to pass here so the span created from this operation will be in its own
+	// trace.
 	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	srv := &server{DB: db}
+	if err := srv.listenAndServe(); err != nil {
 		panic(err)
 	}
 }
