112444f52bf4f1ce809d9a08fb758599e7b8857e
Tyler Yahn <codingalias@gmail.com>
2021-09-28 16:00:25 -0700

    Update build constraints

--- instrumentation/github.com/confluentinc/confluent-kafka-go/kafka/splunkkafka/producer.go
+++ instrumentation/github.com/confluentinc/confluent-kafka-go/kafka/splunkkafka/producer.go
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !windows
-// +build !windows
+//go:build cgo && (linux || darwin)
+// +build cgo
+// +build linux darwin
 
 package splunkkafka
 
