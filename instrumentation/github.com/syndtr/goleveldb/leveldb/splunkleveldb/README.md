916deb5c0adddb13b053f839c053261106ddf244
Tyler Yahn <codingalias@gmail.com>
2021-11-10 13:16:38 -0800

    Add example_test.go

--- instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb/README.md
+++ instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb/README.md
@@ -8,3 +8,9 @@ package.
 
 - Go: 1.15+
 - github.com/syndtr/goleveldb/leveldb: 1.0.0+
+
+## Getting Started
+
+This package is designed to be used as a drop-in replacement for the use of the
+`github.com/syndtr/goleveldb/leveldb` package. Examples of how to do so can be
+found [here](./example_test.go).
---
+++ instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb/example_test.go
@@ -0,0 +1,99 @@
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package splunkleveldb_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb"
+	"github.com/syndtr/goleveldb/leveldb/storage"
+	"go.opentelemetry.io/otel"
+)
+
+func Example() {
+	ctx, span := otel.Tracer("my-inst").Start(context.Background(), "main")
+	defer span.End()
+
+	// Open a new database backed by memory storage.
+	memstore := storage.NewMemStorage()
+	// Ensure span is used as a parent for all spans the database will create.
+	db, err := splunkleveldb.Open(memstore, nil, splunkleveldb.WithContext(ctx))
+	if err != nil {
+		// Assume corruptions and attept a recover.
+		db, err = splunkleveldb.Recover(memstore, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Write to the database. A span is created to trace this operation.
+	if err := db.Put([]byte("key"), []byte("value"), nil); err != nil {
+		log.Println("failed to write data", err)
+	}
+
+	// Read back the data. A span is created to trace this operation.
+	data, err := db.Get([]byte("key"), nil)
+	if err != nil {
+		log.Println("failed to read data", err)
+	} else {
+		log.Println("read from database:", data)
+	}
+
+	// Delete the data for the key. A span is created to trace this operation.
+	if err := db.Delete([]byte("key"), nil); err != nil {
+		log.Println("failed to delete data", err)
+	}
+}
+
+func ExampleOpen() {
+	memstore := storage.NewMemStorage()
+	db, err := splunkleveldb.Open(memstore, nil)
+	if err != nil {
+		// Assume corruptions and attept a recover.
+		db, err = splunkleveldb.Recover(memstore, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
+
+func ExampleOpenFile() {
+	dbPath := "/path/to/db"
+	db, err := splunkleveldb.OpenFile(dbPath, nil)
+	if err != nil {
+		// Assume corruptions and attept a recover.
+		db, err = splunkleveldb.RecoverFile(dbPath, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
