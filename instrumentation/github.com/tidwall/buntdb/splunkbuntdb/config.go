c03218130f75ae9a3d6cde9e9a69b5c09b8aa9e5
Tyler Yahn <MrAlias@users.noreply.github.com>
2021-12-23 10:08:20 -0800

    Update splunkbuntdb to use internal.Config (#290)

--- instrumentation/github.com/tidwall/buntdb/splunkbuntdb/config.go
+++ instrumentation/github.com/tidwall/buntdb/splunkbuntdb/config.go
@@ -17,30 +17,37 @@ package splunkbuntdb
 import (
 	"context"
 
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/codes"
 	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 	"go.opentelemetry.io/otel/trace"
 
-	splunkotel "github.com/signalfx/splunk-otel-go"
+	"github.com/signalfx/splunk-otel-go/instrumentation/internal"
 )
 
 // instrumentationName is the instrumentation library identifier for a Tracer.
 const instrumentationName = "github.com/signalfx/splunk-otel-go/instrumentation/github.com/tidwall/buntdb/splunkbuntdb"
 
 type config struct {
-	ctx              context.Context
-	tracer           trace.Tracer
-	defaultStartOpts []trace.SpanStartOption
+	*internal.Config
+
+	ctx context.Context
 }
 
 func newConfig(options ...Option) *config {
 	c := config{
-		defaultStartOpts: []trace.SpanStartOption{
-			trace.WithSpanKind(trace.SpanKindClient),
-			trace.WithAttributes(semconv.DBSystemKey.String("buntdb")),
-		},
+		Config: internal.NewConfig(instrumentationName, internal.OptionFunc(
+			func(c *internal.Config) {
+				c.DefaultStartOpts = []trace.SpanStartOption{
+					trace.WithAttributes(
+						semconv.DBSystemKey.String("buntdb"),
+						semconv.NetTransportInProc,
+					),
+					// From the specification: span kind MUST always be CLIENT.
+					trace.WithSpanKind(trace.SpanKindClient),
+				}
+			}),
+		),
+		ctx: context.Background(),
 	}
 
 	for _, o := range options {
@@ -49,86 +56,40 @@ func newConfig(options ...Option) *config {
 		}
 	}
 
-	if c.tracer == nil {
-		c.tracer = otel.Tracer(
-			instrumentationName,
-			trace.WithInstrumentationVersion(splunkotel.Version()),
-			trace.WithSchemaURL(semconv.SchemaURL),
-		)
-	}
-
-	if c.ctx == nil {
-		c.ctx = context.Background()
-	}
-
 	return &c
 }
 
 // copy returns a deep copy of c.
 func (c *config) copy() *config {
-	copyOpts := make([]trace.SpanStartOption, len(c.defaultStartOpts))
-	copy(copyOpts, c.defaultStartOpts)
-
-	newCfg := &config{
-		ctx:              c.ctx,
-		tracer:           c.tracer,
-		defaultStartOpts: copyOpts,
+	return &config{
+		Config: c.Config.Copy(),
+		ctx:    c.ctx,
 	}
-	return newCfg
-}
-
-// resolveTracer returns an OTel tracer from the appropriate TracerProvider.
-//
-// If the passed context contains a span, the TracerProvider that created the
-// tracer that created that span will be used. Otherwise, the TracerProvider
-// from c is used.
-func (c *config) resolveTracer(ctx context.Context) trace.Tracer {
-	if span := trace.SpanFromContext(ctx); span.SpanContext().IsValid() {
-		return span.TracerProvider().Tracer(
-			instrumentationName,
-			trace.WithInstrumentationVersion(splunkotel.Version()),
-			trace.WithSchemaURL(semconv.SchemaURL),
-		)
-	}
-	// There is a possibility that the config was not created with newConfig
-	// (i.e. new(Client)), try to handle this situation gracefully.
-	if c == nil || c.tracer == nil {
-		return otel.Tracer(
-			instrumentationName,
-			trace.WithInstrumentationVersion(splunkotel.Version()),
-			trace.WithSchemaURL(semconv.SchemaURL),
-		)
-	}
-	return c.tracer
-}
-
-// withSpan wraps the function f with a span.
-func (c *config) withSpan(spanName string, f func() error, opts ...trace.SpanStartOption) error {
-	var o []trace.SpanStartOption
-	if c == nil || len(c.defaultStartOpts) == 0 {
-		o = make([]trace.SpanStartOption, len(opts))
-		copy(o, opts)
-	} else {
-		o = make([]trace.SpanStartOption, len(c.defaultStartOpts)+len(opts))
-		copy(o, c.defaultStartOpts)
-		copy(o[len(c.defaultStartOpts):], opts)
-	}
-
-	_, span := c.resolveTracer(c.ctx).Start(c.ctx, spanName, o...)
-
-	err := f()
-	if err != nil {
-		span.RecordError(err)
-		span.SetStatus(codes.Error, err.Error())
-	}
-	span.End()
-
-	return err
 }
 
 // Option applies options to a configuration.
 type Option interface {
 	apply(*config)
+}
+
+type optionConv struct {
+	iOpt internal.Option
+}
+
+func (o optionConv) apply(c *config) {
+	o.iOpt.Apply(c.Config)
+}
+
+// WithTracerProvider returns an Option that sets the TracerProvider used with
+// this instrumentation library.
+func WithTracerProvider(tp trace.TracerProvider) Option {
+	return optionConv{iOpt: internal.WithTracerProvider(tp)}
+}
+
+// WithAttributes returns an Option that appends attr to the attributes set
+// for every span created with this instrumentation library.
+func WithAttributes(attr []attribute.KeyValue) Option {
+	return optionConv{iOpt: internal.WithAttributes(attr)}
 }
 
 type optionFunc func(*config)
@@ -137,30 +98,9 @@ func (o optionFunc) apply(c *config) {
 	o(c)
 }
 
-// WithTracerProvider returns an Option that sets the TracerProvider used with
-// this instrumentation library.
-func WithTracerProvider(tp trace.TracerProvider) Option {
-	return optionFunc(func(c *config) {
-		c.tracer = tp.Tracer(
-			instrumentationName,
-			trace.WithInstrumentationVersion(splunkotel.Version()),
-			trace.WithSchemaURL(semconv.SchemaURL),
-		)
-	})
-}
-
-// WithAttributes returns an Option that appends attr to the attributes set
-// for every span created with this instrumentation library.
-func WithAttributes(attr []attribute.KeyValue) Option {
-	return optionFunc(func(c *config) {
-		c.defaultStartOpts = append(
-			c.defaultStartOpts,
-			trace.WithAttributes(attr...),
-		)
-	})
-}
-
-// WithContext sets the parent context for spans.
+// WithContext returns an Option that sets the Context used with this
+// instrumentation library by default. This is used to pass context of any
+// existing trace to the instrumentation.
 func WithContext(ctx context.Context) Option {
 	return optionFunc(func(c *config) {
 		c.ctx = ctx
