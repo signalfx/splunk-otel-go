05c364b7d67761ed3f1afd34882f0d8ae4b7cfa9
Tyler Yahn <codingalias@gmail.com>
2022-01-06 10:35:25 -0800

    Add general use example for v3 and v5

--- instrumentation/gopkg.in/olivere/elastic/splunkelastic/example_test.go
+++ instrumentation/gopkg.in/olivere/elastic/splunkelastic/example_test.go
@@ -14,6 +14,71 @@
 
 package splunkelastic_test
 
-func Example() {
-	// FIXME: implement.
+import (
+	"context"
+	"net/http"
+
+	"github.com/signalfx/splunk-otel-go/instrumentation/gopkg.in/olivere/elastic/splunkelastic"
+	elasticv3 "gopkg.in/olivere/elastic.v3"
+	elasticv5 "gopkg.in/olivere/elastic.v5"
+)
+
+func Example_v5() {
+	// Wrap the HTTP Transport used by the client to communicate with the
+	// Elasticsearch cluster so all requests made are included in traces.
+	client, err := elasticv5.NewClient(
+		elasticv5.SetURL("http://127.0.0.1:9200"),
+		elasticv5.SetHttpClient(&http.Client{
+			Transport: splunkelastic.WrapRoundTripper(http.DefaultTransport),
+		}),
+	)
+	if err != nil {
+		// Handle error.
+		panic(err)
+	}
+
+	// Spans are emitted for all operations the client performs against the
+	// Elasticsearch cluster.
+	_, err = client.Index().
+		Index("twitter").
+		Type("tweet").
+		Index("1").
+		BodyString(`{"user": "test", "message": "hello"}`).
+		// If a context that contains an span is passed here it will be used
+		// as the parent span for the performed operations.
+		Do(context.Background())
+	if err != nil {
+		// Handle error.
+		panic(err)
+	}
+}
+
+func Example_v3() {
+	// Wrap the HTTP Transport used by the client to communicate with the
+	// Elasticsearch cluster so all requests made are included in traces.
+	client, err := elasticv3.NewClient(
+		elasticv3.SetURL("http://127.0.0.1:9200"),
+		elasticv3.SetHttpClient(&http.Client{
+			Transport: splunkelastic.WrapRoundTripper(http.DefaultTransport),
+		}),
+	)
+	if err != nil {
+		// Handle error.
+		panic(err)
+	}
+
+	// Spans are emitted for all operations the client performs against the
+	// Elasticsearch cluster.
+	_, err = client.Index().
+		Index("twitter").
+		Type("tweet").
+		Index("1").
+		BodyString(`{"user": "test", "message": "hello"}`).
+		// Be sure to use DoC to propagate a trace with a context that
+		// contains a parent span.
+		DoC(context.Background())
+	if err != nil {
+		// Handle error.
+		panic(err)
+	}
 }
--- instrumentation/gopkg.in/olivere/elastic/splunkelastic/go.mod
+++ instrumentation/gopkg.in/olivere/elastic/splunkelastic/go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/signalfx/splunk-otel-go v0.0.0-00010101000000-000000000000
 	go.opentelemetry.io/otel v1.3.0
 	go.opentelemetry.io/otel/trace v1.3.0
+	gopkg.in/olivere/elastic.v3 v3.0.75
+	gopkg.in/olivere/elastic.v5 v5.0.86
 )
 
 replace github.com/signalfx/splunk-otel-go => ../../../../../
