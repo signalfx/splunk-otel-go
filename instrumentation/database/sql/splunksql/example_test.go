1d45c043e34564428c1eca2225d9b80565dc0b6f
Tyler Yahn <MrAlias@users.noreply.github.com>
2022-03-25 07:53:13 -0700

    Upgrade all semconv 1.4.0 to 1.7.0 (#657)

--- instrumentation/database/sql/splunksql/example_test.go
+++ instrumentation/database/sql/splunksql/example_test.go
@@ -19,7 +19,7 @@ import (
 	"log"
 
 	"go.opentelemetry.io/otel/attribute"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
 )
