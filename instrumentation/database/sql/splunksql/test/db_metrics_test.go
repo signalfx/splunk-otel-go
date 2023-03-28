674777ffaeae6e4e2e9e1078d519f15e97be5d0e
Robert Pająk <pellared@hotmail.com>
2023-03-28 16:44:08 +0200

    Use metricdatatest

--- instrumentation/database/sql/splunksql/test/db_metrics_test.go
+++ instrumentation/database/sql/splunksql/test/db_metrics_test.go
@@ -17,18 +17,22 @@ package test
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 
+	splunkotel "github.com/signalfx/splunk-otel-go"
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
 )
 
-func TestMetrics(t *testing.T) {
+func TestMetrics(t *testing.T) { //nolint: funlen // big want struct
 	ctx := context.Background()
 	reader := metric.NewManualReader()
 	meterProvider := metric.NewMeterProvider(metric.WithReader(reader))
@@ -50,7 +54,7 @@ func TestMetrics(t *testing.T) {
 		DSNParser: func(string) (splunksql.ConnectionConfig, error) { return connCfg, nil },
 	})
 
-	// create 1 used connection
+	// create 1 connection
 	db, err := splunksql.Open(driverName, "dataSourceName",
 		splunksql.WithMeterProvider(meterProvider))
 	require.NoError(t, err)
@@ -59,35 +63,76 @@ func TestMetrics(t *testing.T) {
 	require.NoError(t, err)
 
 	// assert
+	wantPoolAttr := attribute.String("pool.name", connCfg.ConnectionString)
+	want := metricdata.ScopeMetrics{
+		Scope: instrumentation.Scope{
+			Name:      "github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql",
+			Version:   splunkotel.Version(),
+			SchemaURL: semconv.SchemaURL,
+		},
+		Metrics: []metricdata.Metrics{
+			{
+				Name:        "db.client.connections.usage",
+				Unit:        "{connection}",
+				Description: "The number of connections that are currently in state described by the state attribute",
+				Data: metricdata.Sum[int64]{
+					DataPoints: []metricdata.DataPoint[int64]{
+						{
+							Attributes: attribute.NewSet(
+								wantPoolAttr,
+								attribute.String("state", "used"),
+							),
+							Value: 0,
+						},
+						{
+							Attributes: attribute.NewSet(
+								wantPoolAttr,
+								attribute.String("state", "idle"),
+							),
+							Value: 1,
+						},
+					},
+					Temporality: metricdata.CumulativeTemporality,
+					IsMonotonic: false,
+				},
+			},
+			{
+				Name:        "db.client.connections.max",
+				Unit:        "{connection}",
+				Description: "The maximum number of open connections allowed",
+				Data: metricdata.Sum[int64]{
+					DataPoints: []metricdata.DataPoint[int64]{
+						{
+							Attributes: attribute.NewSet(wantPoolAttr),
+							Value:      0,
+						},
+					},
+					Temporality: metricdata.CumulativeTemporality,
+					IsMonotonic: false,
+				},
+			},
+			{
+				Name:        "db.client.connections.wait_time",
+				Unit:        "ms",
+				Description: "The time it took to obtain an open connection from the pool",
+				Data: metricdata.Sum[int64]{
+					DataPoints: []metricdata.DataPoint[int64]{
+						{
+							Attributes: attribute.NewSet(wantPoolAttr),
+							Value:      0,
+						},
+					},
+					Temporality: metricdata.CumulativeTemporality,
+					IsMonotonic: false,
+				},
+			},
+		},
+	}
+
 	rm := metricdata.ResourceMetrics{}
 	err = reader.Collect(ctx, &rm)
 	require.NoError(t, err)
 	require.Len(t, rm.ScopeMetrics, 1, "should export metrics")
-	metrics := rm.ScopeMetrics[0]
-
-	assertMetrics(t, metrics, "db.client.connections.usage", func(m metricdata.Metrics) {
-		assert.Equal(t, "{connection}", m.Unit)
-	})
-
-	// log all exported metrics
-	d, err := json.Marshal(metrics.Metrics)
-	require.NoError(t, err)
-	t.Log(string(d))
-}
-
-func assertMetrics(t *testing.T, got metricdata.ScopeMetrics, name string, fn func(metricdata.Metrics)) {
-	t.Helper()
-	for _, m := range got.Metrics {
-		if m.Name == name {
-			fn(m)
-			return
-		}
-	}
-
-	// Not found. Generate assertion failure.
-	var gotMetrics []string
-	for _, m := range got.Metrics {
-		gotMetrics = append(gotMetrics, m.Name)
-	}
-	assert.Failf(t, "should contain metric", "want: %v, got: %v", name, gotMetrics)
+	got := rm.ScopeMetrics[0]
+	metricdatatest.AssertEqual(t, want, got, metricdatatest.IgnoreTimestamp())
 }
--- instrumentation/github.com/go-sql-driver/mysql/splunkmysql/test/metrics_test.go
+++ instrumentation/github.com/go-sql-driver/mysql/splunkmysql/test/metrics_test.go
@@ -16,14 +16,18 @@ package test
 
 import (
 	"context"
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 
+	splunkotel "github.com/signalfx/splunk-otel-go"
 	"github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql"
 	_ "github.com/signalfx/splunk-otel-go/instrumentation/github.com/go-sql-driver/mysql/splunkmysql"
 )
@@ -41,35 +45,76 @@ func TestMetrics(t *testing.T) {
 	require.NoError(t, db.Ping())
 
 	// assert
+	wantPoolAttr := attribute.String("pool.name", dsnSanitized)
+	want := metricdata.ScopeMetrics{
+		Scope: instrumentation.Scope{
+			Name:      "github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql",
+			Version:   splunkotel.Version(),
+			SchemaURL: semconv.SchemaURL,
+		},
+		Metrics: []metricdata.Metrics{
+			{
+				Name:        "db.client.connections.usage",
+				Unit:        "{connection}",
+				Description: "The number of connections that are currently in state described by the state attribute",
+				Data: metricdata.Sum[int64]{
+					DataPoints: []metricdata.DataPoint[int64]{
+						{
+							Attributes: attribute.NewSet(
+								wantPoolAttr,
+								attribute.String("state", "used"),
+							),
+							Value: 0,
+						},
+						{
+							Attributes: attribute.NewSet(
+								wantPoolAttr,
+								attribute.String("state", "idle"),
+							),
+							Value: 1,
+						},
+					},
+					Temporality: metricdata.CumulativeTemporality,
+					IsMonotonic: false,
+				},
+			},
+			{
+				Name:        "db.client.connections.max",
+				Unit:        "{connection}",
+				Description: "The maximum number of open connections allowed",
+				Data: metricdata.Sum[int64]{
+					DataPoints: []metricdata.DataPoint[int64]{
+						{
+							Attributes: attribute.NewSet(wantPoolAttr),
+							Value:      0,
+						},
+					},
+					Temporality: metricdata.CumulativeTemporality,
+					IsMonotonic: false,
+				},
+			},
+			{
+				Name:        "db.client.connections.wait_time",
+				Unit:        "ms",
+				Description: "The time it took to obtain an open connection from the pool",
+				Data: metricdata.Sum[int64]{
+					DataPoints: []metricdata.DataPoint[int64]{
+						{
+							Attributes: attribute.NewSet(wantPoolAttr),
+							Value:      0,
+						},
+					},
+					Temporality: metricdata.CumulativeTemporality,
+					IsMonotonic: false,
+				},
+			},
+		},
+	}
+
 	rm := metricdata.ResourceMetrics{}
 	err = reader.Collect(ctx, &rm)
 	require.NoError(t, err)
 	require.Len(t, rm.ScopeMetrics, 1, "should export metrics")
-	metrics := rm.ScopeMetrics[0]
-
-	assertMetrics(t, metrics, "db.client.connections.usage", func(m metricdata.Metrics) {
-		assert.Equal(t, "{connection}", m.Unit)
-	})
-
-	// log all exported metrics
-	d, err := json.Marshal(metrics.Metrics)
-	require.NoError(t, err)
-	t.Log(string(d))
-}
-
-func assertMetrics(t *testing.T, got metricdata.ScopeMetrics, name string, fn func(metricdata.Metrics)) {
-	t.Helper()
-	for _, m := range got.Metrics {
-		if m.Name == name {
-			fn(m)
-			return
-		}
-	}
-
-	// Not found. Generate assertion failure.
-	var gotMetrics []string
-	for _, m := range got.Metrics {
-		gotMetrics = append(gotMetrics, m.Name)
-	}
-	assert.Failf(t, "should contain metric", "want: %v, got: %v", name, gotMetrics)
+	got := rm.ScopeMetrics[0]
+	metricdatatest.AssertEqual(t, want, got, metricdatatest.IgnoreTimestamp())
 }
