66dd4ca0f8526f3e150eeb745ae81ab9edb4183f
Robert Pająk <pellared@hotmail.com>
2023-03-29 09:53:31 +0200

    Improve test coverage

--- instrumentation/database/sql/splunksql/test/db_metrics_test.go
+++ instrumentation/database/sql/splunksql/test/db_metrics_test.go
@@ -33,106 +33,122 @@ import (
 )
 
 func TestMetrics(t *testing.T) { //nolint: funlen // big want struct
-	ctx := context.Background()
-	reader := metric.NewManualReader()
-	meterProvider := metric.NewMeterProvider(metric.WithReader(reader))
-	defer func() { assert.NoError(t, meterProvider.Shutdown(ctx)) }()
-
-	driverName := "simple-driver"
-	driver := newSimpleMockDriver()
-	connCfg := splunksql.ConnectionConfig{
-		// Do not set the Name field so monikers are used to identify
-		// spans.
-		ConnectionString: "mockDB://bob@localhost:8080/testDB",
-		User:             "bob",
-		Host:             "localhost",
-		Port:             8080,
-		NetTransport:     splunksql.NetTransportOther,
-	}
-	sql.Register(driverName, driver)
-	splunksql.Register(driverName, splunksql.InstrumentationConfig{
-		DSNParser: func(string) (splunksql.ConnectionConfig, error) { return connCfg, nil },
-	})
-
-	// create 1 connection
-	db, err := splunksql.Open(driverName, "dataSourceName",
-		splunksql.WithMeterProvider(meterProvider))
-	require.NoError(t, err)
-	defer func() { assert.NoError(t, db.Close()) }()
-	_, err = db.Exec("SELECT 1")
-	require.NoError(t, err)
-
-	// assert
-	wantPoolAttr := attribute.String("pool.name", connCfg.ConnectionString)
-	want := metricdata.ScopeMetrics{
-		Scope: instrumentation.Scope{
-			Name:      "github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql",
-			Version:   splunkotel.Version(),
-			SchemaURL: semconv.SchemaURL,
+	testCases := []struct {
+		driverName       string
+		connectionString string
+		wantPoolName     string
+	}{
+		{
+			driverName:       "SanitizedConnectionString",
+			connectionString: "mockDB://bob@localhost:8080/testDB",
+			wantPoolName:     "mockDB://bob@localhost:8080/testDB",
 		},
-		Metrics: []metricdata.Metrics{
-			{
-				Name:        "db.client.connections.usage",
-				Unit:        "{connection}",
-				Description: "The number of connections that are currently in state described by the state attribute",
-				Data: metricdata.Sum[int64]{
-					DataPoints: []metricdata.DataPoint[int64]{
-						{
-							Attributes: attribute.NewSet(
-								wantPoolAttr,
-								attribute.String("state", "used"),
-							),
-							Value: 0,
-						},
-						{
-							Attributes: attribute.NewSet(
-								wantPoolAttr,
-								attribute.String("state", "idle"),
-							),
-							Value: 1,
-						},
-					},
-					Temporality: metricdata.CumulativeTemporality,
-					IsMonotonic: false,
-				},
-			},
-			{
-				Name:        "db.client.connections.max",
-				Unit:        "{connection}",
-				Description: "The maximum number of open connections allowed",
-				Data: metricdata.Sum[int64]{
-					DataPoints: []metricdata.DataPoint[int64]{
-						{
-							Attributes: attribute.NewSet(wantPoolAttr),
-							Value:      0,
-						},
-					},
-					Temporality: metricdata.CumulativeTemporality,
-					IsMonotonic: false,
-				},
-			},
-			{
-				Name:        "db.client.connections.wait_time",
-				Unit:        "ms",
-				Description: "The time it took to obtain an open connection from the pool",
-				Data: metricdata.Sum[int64]{
-					DataPoints: []metricdata.DataPoint[int64]{
-						{
-							Attributes: attribute.NewSet(wantPoolAttr),
-							Value:      0,
-						},
-					},
-					Temporality: metricdata.CumulativeTemporality,
-					IsMonotonic: false,
-				},
-			},
+		{
+			driverName:       "NoConnectionString",
+			connectionString: "",
+			wantPoolName:     "NoConnectionString",
 		},
 	}
+	for _, tc := range testCases {
+		t.Run(tc.driverName, func(t *testing.T) {
+			// arrange
+			ctx := context.Background()
+			reader := metric.NewManualReader()
+			meterProvider := metric.NewMeterProvider(metric.WithReader(reader))
+			defer func() { assert.NoError(t, meterProvider.Shutdown(ctx)) }()
 
-	rm := metricdata.ResourceMetrics{}
-	err = reader.Collect(ctx, &rm)
-	require.NoError(t, err)
-	require.Len(t, rm.ScopeMetrics, 1, "should export metrics")
-	got := rm.ScopeMetrics[0]
-	metricdatatest.AssertEqual(t, want, got, metricdatatest.IgnoreTimestamp())
+			// instrument: register the fake driver
+			driver := newSimpleMockDriver()
+			connCfg := splunksql.ConnectionConfig{
+				ConnectionString: tc.connectionString,
+				Host:             "localhost", // to avoid errors in logs
+			}
+			sql.Register(tc.driverName, driver)
+			splunksql.Register(tc.driverName, splunksql.InstrumentationConfig{
+				DSNParser: func(string) (splunksql.ConnectionConfig, error) { return connCfg, nil },
+			})
+
+			// create 1 connection
+			db, err := splunksql.Open(tc.driverName, "dataSourceName",
+				splunksql.WithMeterProvider(meterProvider))
+			require.NoError(t, err)
+			defer func() { assert.NoError(t, db.Close()) }()
+			_, err = db.Exec("SELECT 1")
+			require.NoError(t, err)
+
+			// assert
+			wantPoolAttr := attribute.String("pool.name", tc.wantPoolName)
+			want := metricdata.ScopeMetrics{
+				Scope: instrumentation.Scope{
+					Name:      "github.com/signalfx/splunk-otel-go/instrumentation/database/sql/splunksql",
+					Version:   splunkotel.Version(),
+					SchemaURL: semconv.SchemaURL,
+				},
+				Metrics: []metricdata.Metrics{
+					{
+						Name:        "db.client.connections.usage",
+						Unit:        "{connection}",
+						Description: "The number of connections that are currently in state described by the state attribute",
+						Data: metricdata.Sum[int64]{
+							DataPoints: []metricdata.DataPoint[int64]{
+								{
+									Attributes: attribute.NewSet(
+										wantPoolAttr,
+										attribute.String("state", "used"),
+									),
+									Value: 0,
+								},
+								{
+									Attributes: attribute.NewSet(
+										wantPoolAttr,
+										attribute.String("state", "idle"),
+									),
+									Value: 1,
+								},
+							},
+							Temporality: metricdata.CumulativeTemporality,
+							IsMonotonic: false,
+						},
+					},
+					{
+						Name:        "db.client.connections.max",
+						Unit:        "{connection}",
+						Description: "The maximum number of open connections allowed",
+						Data: metricdata.Sum[int64]{
+							DataPoints: []metricdata.DataPoint[int64]{
+								{
+									Attributes: attribute.NewSet(wantPoolAttr),
+									Value:      0,
+								},
+							},
+							Temporality: metricdata.CumulativeTemporality,
+							IsMonotonic: false,
+						},
+					},
+					{
+						Name:        "db.client.connections.wait_time",
+						Unit:        "ms",
+						Description: "The time it took to obtain an open connection from the pool",
+						Data: metricdata.Sum[int64]{
+							DataPoints: []metricdata.DataPoint[int64]{
+								{
+									Attributes: attribute.NewSet(wantPoolAttr),
+									Value:      0,
+								},
+							},
+							Temporality: metricdata.CumulativeTemporality,
+							IsMonotonic: false,
+						},
+					},
+				},
+			}
+
+			rm := metricdata.ResourceMetrics{}
+			err = reader.Collect(ctx, &rm)
+			require.NoError(t, err)
+			require.Len(t, rm.ScopeMetrics, 1, "should export metrics")
+			got := rm.ScopeMetrics[0]
+			metricdatatest.AssertEqual(t, want, got, metricdatatest.IgnoreTimestamp())
+		})
+	}
 }
