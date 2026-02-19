9c53b7b3c3fa9d8d7cbbb7649950c5037938fb3f
Robert Pająk <rpajak@splunk.com>
2026-02-19 17:11:02 +0100

    Remove deprecated module comment in example_test.go
    Removed deprecated module comment from the test file.

--- instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql/example_test.go
+++ instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql/example_test.go
@@ -20,7 +20,6 @@ import (
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 
-	//nolint:staticcheck // Deprecated module, but still used in this test.
 	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql"
 )
 
