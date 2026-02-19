9a2da816461aeff855a6d94d54635d3a555eaae7
Robert Pająk <rpajak@splunk.com>
2026-02-19 18:26:58 +0100

    Update instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb/test/splunkleveldb_test.go

--- instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb/test/splunkleveldb_test.go
+++ instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb/test/splunkleveldb_test.go
@@ -33,7 +33,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	traceapi "go.opentelemetry.io/otel/trace"
 
-	//nolint:staticcheck // Deprecated module, but still used in this test.
+	//nolint:staticcheck // Deprecated package, but still used here.
 	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/syndtr/goleveldb/leveldb/splunkleveldb"
 )
 
