0fffe92346568c81d314efc86ac8f1377c136120
Robert Pająk <rpajak@splunk.com>
2026-02-19 17:11:33 +0100

    Add comment for deprecated module usage
    Added a comment to indicate the use of a deprecated module.

--- instrumentation/github.com/gomodule/redigo/splunkredigo/redis/conn.go
+++ instrumentation/github.com/gomodule/redigo/splunkredigo/redis/conn.go
@@ -27,6 +27,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
 
+	//nolint:staticcheck // Deprecated module, but still used in this test.
 	splunkredigo "github.com/signalfx/splunk-otel-go/instrumentation/github.com/gomodule/redigo/splunkredigo"
 	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/gomodule/redigo/splunkredigo/option"
 	"github.com/signalfx/splunk-otel-go/instrumentation/internal"
