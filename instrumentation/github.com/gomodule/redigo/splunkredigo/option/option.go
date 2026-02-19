aade093f3a9232460568b138837b1458ddd5ff4d
Robert Pająk <rpajak@splunk.com>
2026-02-19 17:28:31 +0100

    Mark splunkredigo module as deprecated
    Add deprecation notice for the splunkredigo module.

--- instrumentation/github.com/gomodule/redigo/splunkredigo/option/option.go
+++ instrumentation/github.com/gomodule/redigo/splunkredigo/option/option.go
@@ -14,6 +14,9 @@
 
 // Package option provides configuration options for the splunkredigo package
 // and its subpackages.
+//
+// Deprecated: the module is not going to be released in future.
+// See https://github.com/signalfx/splunk-otel-go/issues/4397 for more details
 package option
 
 import (
