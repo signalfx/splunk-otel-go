55019fee640a1c112d50c2df8f04c87d87879806
Robert Pająk <rpajak@splunk.com>
2026-02-19 21:45:41 +0100

    Apply suggestions from code review

--- instrumentation/github.com/miekg/dns/splunkdns/dns.go
+++ instrumentation/github.com/miekg/dns/splunkdns/dns.go
@@ -15,7 +15,7 @@
 // Package splunkdns provides OpenTelemetry instrumentation for the
 // github.com/miekg/dns package.
 //
-// Deprecated: the module is not going to be released in future.
+// Deprecated: this module is no longer supported.
 // See https://github.com/signalfx/splunk-otel-go/issues/4400 for more details.
 package splunkdns
 
