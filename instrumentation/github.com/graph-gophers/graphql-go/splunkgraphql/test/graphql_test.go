eab166a1dd2e1f0d8a632abbcf04055df70c7a18
Robert Pająk <rpajak@splunk.com>
2026-02-19 18:30:42 +0100

    Update instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql/test/graphql_test.go

--- instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql/test/graphql_test.go
+++ instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql/test/graphql_test.go
@@ -38,7 +38,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	traceapi "go.opentelemetry.io/otel/trace"
 
-	//nolint:staticcheck // Deprecated module, but still used in this test.
+	//nolint:staticcheck // Deprecated package, but still used here.
 	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql"
 	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/graph-gophers/graphql-go/splunkgraphql/internal"
 )
