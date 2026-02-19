f99fb0f21aa18666eaebf16403eb5513335525a3
Robert Pająk <rpajak@splunk.com>
2026-02-19 18:24:15 +0100

    Update instrumentation/gopkg.in/olivere/elastic/splunkelastic/test/elastic_test.go

--- instrumentation/gopkg.in/olivere/elastic/splunkelastic/test/elastic_test.go
+++ instrumentation/gopkg.in/olivere/elastic/splunkelastic/test/elastic_test.go
@@ -39,7 +39,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	apitrace "go.opentelemetry.io/otel/trace"
 
-	//nolint:staticcheck // Deprecated module, but still used in this test.
+	//nolint:staticcheck // Deprecated package, but still used here.
 	"github.com/signalfx/splunk-otel-go/instrumentation/gopkg.in/olivere/elastic/splunkelastic"
 )
 
