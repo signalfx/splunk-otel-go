6516eb500c58627e8025c16cfd36dc387619eb99
Robert Pajak <pellared@hotmail.com>
2026-02-19 17:07:54 +0100

    Deprecate splunkbuntdb

--- instrumentation/github.com/tidwall/buntdb/splunkbuntdb/go.mod
+++ instrumentation/github.com/tidwall/buntdb/splunkbuntdb/go.mod
@@ -1,3 +1,5 @@
+// Deprecated: the module is not going to be released in future.
+// See https://github.com/signalfx/splunk-otel-go/issues/4402 for more details.
 module github.com/signalfx/splunk-otel-go/instrumentation/github.com/tidwall/buntdb/splunkbuntdb
 
 go 1.24.0
--- instrumentation/github.com/tidwall/buntdb/splunkbuntdb/test/buntdb_test.go
+++ instrumentation/github.com/tidwall/buntdb/splunkbuntdb/test/buntdb_test.go
@@ -30,6 +30,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	traceapi "go.opentelemetry.io/otel/trace"
 
+	//nolint:staticcheck // Deprecated module, but still used in this test.
 	"github.com/signalfx/splunk-otel-go/instrumentation/github.com/tidwall/buntdb/splunkbuntdb"
 )
 
