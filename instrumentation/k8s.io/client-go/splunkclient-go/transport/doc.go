70e73f6bf55b2bf89122d7ca7d82ad968ed1155d
Robert Pająk <rpajak@splunk.com>
2026-02-19 17:20:13 +0100

    Mark transport package as deprecated
    Added deprecation notice for the transport package.

--- instrumentation/k8s.io/client-go/splunkclient-go/transport/doc.go
+++ instrumentation/k8s.io/client-go/splunkclient-go/transport/doc.go
@@ -14,4 +14,7 @@
 
 // Package transport provides a Kubernetes client wrapper function that traces
 // the Kubernetes client operations.
+//
+// Deprecated: the module is not going to be released in future.
+// See https://github.com/signalfx/splunk-otel-go/issues/4403 for more details.
 package transport
