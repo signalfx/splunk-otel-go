d37f230ad3e871e79c93992ae967b6fe5f41352e
Robert Pajak <pellared@hotmail.com>
2026-05-28 09:38:10 +0200

    lint

--- instrumentation/gopkg.in/olivere/elastic/splunkelastic/tokenize_test.go
+++ instrumentation/gopkg.in/olivere/elastic/splunkelastic/tokenize_test.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//nolint:goconst // This test exercises generated spec paths and intentionally keeps repeated literals inline.
 package splunkelastic
 
 import (
