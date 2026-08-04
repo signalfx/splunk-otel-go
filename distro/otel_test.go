7e21647b6b92ab133c4aa0343381d9252492d619
Tyler Yahn <tyahn@cisco.com>
2026-08-04 19:35:19 +0000

    Generate test TLS certificates at runtime
    The checked-in test certificate expired and caused TLS exporter tests to
    hang until their timeout. Generate a self-signed certificate once per test
    process so the tests retain certificate verification without depending on
    a calendar-bound fixture.

--- distro/otel_test.go
+++ distro/otel_test.go
@@ -17,14 +17,19 @@ package distro_test
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
 	"net"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/go-logr/logr/testr"
 	"github.com/stretchr/testify/assert"
@@ -55,93 +60,12 @@ const (
 	metricName = "test_instrument"
 	logBody    = "test_log_body"
 	token      = "secret token"
+)
 
-	testCert = `
------BEGIN CERTIFICATE-----
-MIID9zCCAl+gAwIBAgIQLIENzVUKhcPL/ej83SYlLDANBgkqhkiG9w0BAQsFADBN
-MR4wHAYDVQQKExVta2NlcnQgZGV2ZWxvcG1lbnQgQ0ExETAPBgNVBAsMCHR5bGVy
-QHhpMRgwFgYDVQQDDA9ta2NlcnQgdHlsZXJAeGkwHhcNMjQwNDI1MjEwODQwWhcN
-MjYwNzI1MjEwODQwWjA8MScwJQYDVQQKEx5ta2NlcnQgZGV2ZWxvcG1lbnQgY2Vy
-dGlmaWNhdGUxETAPBgNVBAsMCHR5bGVyQHhpMIIBIjANBgkqhkiG9w0BAQEFAAOC
-AQ8AMIIBCgKCAQEA1J3ecYw+MKsUJ1VBo7mI6haEDdPhzlEmsChbCjdoiiEMY0j0
-sY/LNuwNXB09bN/29tL5Do3fNMtWY0VbPbYhmghsVCd3HWPUApv3aFg0xbXv/kzj
-Y6TXdCf5cN14yYmUPUWHstS4bFbMIri53+etysub/MVPjVOJ9jM7TIpn7wz3GlN+
-i58+ATh6ZMA/HGjyyskKJV0YmWK7BydQdZx1tuFCkN95bOdFtv1fwfR867729f8q
-m623ZGLoShoS6h0++KmXHNW6u/8wewSVJPFiKVpm6i2PUAWgKXjfEJF9l4Yl/fC5
-UBD1svjdZCe1lf3fP9t69tBUZ+rb6eo8o4JrPQIDAQABo2QwYjAOBgNVHQ8BAf8E
-BAMCBaAwEwYDVR0lBAwwCgYIKwYBBQUHAwEwHwYDVR0jBBgwFoAU2gNuInft33MS
-xs9WWFK7nNiq4mIwGgYDVR0RBBMwEYIJbG9jYWxob3N0hwR/AAABMA0GCSqGSIb3
-DQEBCwUAA4IBgQBwm/3jTM/eTH6lvGflue36LnH+ue/OmxqohYLL1Z51l/df34+5
-3XsmGxRMBnQB+IR00Kp0YZGAF7HRFUP4jBWLs96kocK/ZMCcxyW1/rIGIbpSnke3
-ti5GiE2FbpvgdJ0t8Pu7ueQmypp+EcjIfru51sGrKF4WQep0agoGgQrHpSkRKujR
-1S43xudo9sgUVxsPPogHZP+Vh1rjTrd6AWUF921Nda5x+e69e8LAlQLrmNjptTOM
-GuTWc9dKWnBUSV1Nkf3QOCsUkxiXv6BOMJTKi1m4pvLoODEcqKdzwTFR2ziTu9UF
-I92ULCM9Xkvu3nM5NBwnoM8COvAdb3klsk9HDFLSTRToEXCiY/j3xSGVon+JxN5z
-SXCtGTMcKKOY/y3tNP2t3/AqX6YgW2PMzUq6ewq0rAts4mv16T2yQT9PRriPuNv1
-ftjtC8CTT4UN26863lF6utNMmQTKGpwJkStt6Ay+f7KpP2v90C/n4+eZdhJ3hygJ
-xX4bkXYzAksKstc=
------END CERTIFICATE-----
-`
-
-	testCertKey = `
------BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUnd5xjD4wqxQn
-VUGjuYjqFoQN0+HOUSawKFsKN2iKIQxjSPSxj8s27A1cHT1s3/b20vkOjd80y1Zj
-RVs9tiGaCGxUJ3cdY9QCm/doWDTFte/+TONjpNd0J/lw3XjJiZQ9RYey1LhsVswi
-uLnf563Ky5v8xU+NU4n2MztMimfvDPcaU36Lnz4BOHpkwD8caPLKyQolXRiZYrsH
-J1B1nHW24UKQ33ls50W2/V/B9Hzrvvb1/yqbrbdkYuhKGhLqHT74qZcc1bq7/zB7
-BJUk8WIpWmbqLY9QBaApeN8QkX2XhiX98LlQEPWy+N1kJ7WV/d8/23r20FRn6tvp
-6jyjgms9AgMBAAECggEBALvWWQ5B0zOWAPEa7imiIWaFy9aXiVeH9EEO9hdZij72
-IYaZjqxMGEGGW+s0Xe8REpNPIf+MsVRoDAMZ5BjqDleR93qFf0N0zXocpoAF5WGC
-cetdrEp8+GfDZSkkULDknhVflxoTunbkW+aVAongmXafWUkAXF7EHg9nhY0Cff3+
-/xjHmgc/3see9gJuyVlfL4c9EBYwWbKbNX+O5cZikHlz4goaQJUPSQXGnWjZBwEG
-TQEgKN4YrufHgfw6BH1r34H9HoteeIb2ITw2C53SaSenrL97H6o3xSw+PUxbjCfz
-wYc0VIDdmHyP1QgNfqQVxMc7PNkf2jCc3QGaEfL72ZUCgYEA6RTbD/1ZalUSIFpC
-+7CsVrNF8P3GyfpU7gTovFshdL5cBrSYfKeAXGR41O9sutOzS5g+uFrhoTqPcfJ7
-DFU3T/0aB7CipRZmzKEqaJFbVNy8vtny2GX/h8gnS9frDeu018baB3UJ823ZXlKy
-ngETkb56fp6yqfpotxIsfuYRdFMCgYEA6YXfNdZmqST1mz0SdE7/xcxDuBoND2Bg
-D+3YqoPFvIoNYxjLAFQ65STNFbr5nwE2LpJ2zCreXFAPrx2LfQbtRyb6IQ49BAQ2
-WfaIgWho/z45ggZMm46PE2WmX1vlAKLtJmQXWtb48Upw0FcUjxwZvvhAJ+GLyTnP
-4mB2VOW3sC8CgYEAlQYeofSMLO5DbqgHV8E4Dx8EQvcfGJiToLNG0tc5ZtknIapq
-LJkz+t7KWurSDAm7A0ROJbUFuf4lJOeyQMpjWSMsd2o6M29FPuR0pvL5ACM6EpRr
-LmzYmkogxd9BjF79+1BKqqXsPNYpOqOJTHjHuFGfJEH3XbKbLSHTjTcwRb0CgYBn
-yE1JGf4KEhjtYxj+g9V8TKmT+k/vk39d1PDD50hL4HbOocgmX0jFabOTgsNJfTpd
-PE57slmcYSB3TQQfDqMJSjND2ZYYYN7e4IgOKx7uwPLB7JbDU3oWvshP/QErZT1M
-IJOYlY2RfungTbMfXve6PY4Vq1F6nqzbCM/OL0GRlQKBgH2IGS9GhdpcHS+4klBc
-sFB7sIzs0jeCXzN//LxO10DdWgXYZ1eQZLsoOedlcsINH0c2LqI0BurUAUtk+t/f
-hbjvdnAd9fjGwO5d0v+CucVB4ULfLy6Av9INvGXysuGGnxKbnW1lYYHFWcXKHpaJ
-LIGDl/wyWm7itS/QV3P2EA/q
------END PRIVATE KEY-----
-`
-
-	testCA = `
------BEGIN CERTIFICATE-----
-MIIEajCCAtKgAwIBAgIRAPVnMeZtEZ+ZXDBvKQcEFE4wDQYJKoZIhvcNAQELBQAw
-TTEeMBwGA1UEChMVbWtjZXJ0IGRldmVsb3BtZW50IENBMREwDwYDVQQLDAh0eWxl
-ckB4aTEYMBYGA1UEAwwPbWtjZXJ0IHR5bGVyQHhpMB4XDTIwMDgyNDE4MzQzNloX
-DTMwMDgyNDE4MzQzNlowTTEeMBwGA1UEChMVbWtjZXJ0IGRldmVsb3BtZW50IENB
-MREwDwYDVQQLDAh0eWxlckB4aTEYMBYGA1UEAwwPbWtjZXJ0IHR5bGVyQHhpMIIB
-ojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAue009g17hNKKdgnex4Nc/mhc
-AlyB/5XgRd3B29gOKnzlmsZsT+SMDSDABYfCwfrBFJQgnUI/La8zGpB0Ndq1tYNm
-0Pd0qcyhyPNlVQiz2Pbxaw+5h7WEwLaYQOAHLbkb+htWwV8oz+AtWM+79Np5pmfo
-x0dc2rJQnU1Eu7XBn9xA9rcNkU1Sc70D7ghDfBztAXCPn4NyQ6sp17z3WJFkoes1
-Vf9+ri+MiiuA+FCEn5QHYfnN6dSExaEHDmaBa6ktC4Uqyi+VyXTWLgf3kgSzwww0
-80SpBqzuoK09UKqiZQty2rQd2OvsgAWBqW3WB/1WOTfoCoZh/kwhMdQJKPXfBFmw
-TlFVfKcCpbc4K+NXFTnXxS8fW8alzsZaHp/FrZxYO2/YJt6FwkIQ3Afum4PRzrOh
-jISSg7++dp6umF0aIC8MxcIKxrv7qUnHy7pTKE6S9MtU3/gmMZntg6Xza2U5SwuY
-E8z61i9XT3t0hzfWpt1Eqd7nVk182tpUGb++Ii3PAgMBAAGjRTBDMA4GA1UdDwEB
-/wQEAwICBDASBgNVHRMBAf8ECDAGAQH/AgEAMB0GA1UdDgQWBBTaA24id+3fcxLG
-z1ZYUruc2KriYjANBgkqhkiG9w0BAQsFAAOCAYEAQtZkGMBKgsA+rT2xm+NyVH4k
-9eslmp79sGPg3fSwxwTQtvEt+c7Pdeam99NpuUpwcRW/gmXnMXeiw5CFcv7IwZlo
-7ZWSFZdBJUNO4Ed4eiXnpM+2x0TeVhkA+pwHWd5LEoiV87ZlbmPzHByTt1O67t84
-LuhIJ3T+SdI3FT7q6P2vlyvIeIxO/qeAP+9UrWUlkPdKWITDVG/UzAcpmsYrKlJq
-+NVTYv9e07kI9ZmIu5yiXrxcg3M5mmoE8Lr1v6KhzhP00A5S62Pmb/U+m0LOnBEb
-AvRbJMzvwawUrhjxaEbHP3f2mSMjtkH7tFYIwyKfTpJ/Vg6C0u5rnqu2coVPqHyz
-GiS1IV8/EkWTW14APbVVZuyZ+24HJj2o5HtjasRN5tCpfpZhuZeEMNkaAJEZ2Vq0
-Y88rFHHMBaV0M4EkH2YrvygPLsCgdhChCLW1tHuLTrrrDaB1wS/3e/zZIZtWoo1Y
-zuJq2rX0p+GLO/JYG3yUpFCfR/BZbPWnGmNlDB55
------END CERTIFICATE-----
-`
+var (
+	testTLSCert     tls.Certificate
+	testTLSCertErr  error
+	testTLSCertOnce sync.Once
 )
 
 func TestMain(m *testing.M) {
@@ -883,8 +807,9 @@ func reqHander() (<-chan *http.Request, http.HandlerFunc) {
 }
 
 func clientTLSConfig(t *testing.T) *tls.Config {
+	cert := testTLSCertificate(t)
 	certs := x509.NewCertPool()
-	require.True(t, certs.AppendCertsFromPEM([]byte(testCA)), "failed to add CA")
+	certs.AddCert(cert.Leaf)
 
 	return &tls.Config{
 		RootCAs:    certs,
@@ -893,13 +818,62 @@ func clientTLSConfig(t *testing.T) *tls.Config {
 }
 
 func serverTLSConfig(t *testing.T) *tls.Config {
-	cert, err := tls.X509KeyPair([]byte(testCert), []byte(testCertKey))
-	require.NoError(t, err, "failed to load X509 key pair")
-
 	return &tls.Config{
-		Certificates: []tls.Certificate{cert},
+		Certificates: []tls.Certificate{testTLSCertificate(t)},
 		MinVersion:   tls.VersionTLS13,
 	}
+}
+
+func testTLSCertificate(t *testing.T) tls.Certificate {
+	t.Helper()
+
+	testTLSCertOnce.Do(func() {
+		privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			testTLSCertErr = err
+			return
+		}
+
+		serialLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+		serialNumber, err := rand.Int(rand.Reader, serialLimit)
+		if err != nil {
+			testTLSCertErr = err
+			return
+		}
+
+		now := time.Now()
+		template := x509.Certificate{
+			SerialNumber: serialNumber,
+			Subject:      pkix.Name{CommonName: "localhost"},
+			NotBefore:    now.Add(-time.Minute),
+			NotAfter:     now.Add(24 * time.Hour),
+			KeyUsage:     x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+			ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+			DNSNames:     []string{"localhost"},
+			IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
+		}
+
+		certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
+		if err != nil {
+			testTLSCertErr = err
+			return
+		}
+
+		leaf, err := x509.ParseCertificate(certDER)
+		if err != nil {
+			testTLSCertErr = err
+			return
+		}
+
+		testTLSCert = tls.Certificate{
+			Certificate: [][]byte{certDER},
+			PrivateKey:  privateKey,
+			Leaf:        leaf,
+		}
+	})
+
+	require.NoError(t, testTLSCertErr, "failed to generate test TLS certificate")
+	return testTLSCert
 }
 
 func emitSpan(t *testing.T, opts ...distro.Option) {
